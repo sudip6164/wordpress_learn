1f7d6aacbcc4498fa9ea0bea52099ecd8ed30fd4
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-23 14:32:48 +0545

    index.php

--- wp-content/themes/learning/index.php
+++ wp-content/themes/learning/index.php
@@ -1,5 +1,9 @@
-<?php
-// Silence is golden.
-?>
-
-    <main>Welcome</main>
+<!-- index.php is the only other strictly required file. Its job is to render all the front-end output for our theme. -->
+<!DOCTYPE html>
+<html <?php language_attributes();?>>
+<head>
+    <meta charset="<?php bloginfo();?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head();?> <!-- Allows wordpress and plugins to directly insert code into header. -->
+</head>
+<body <?php body_class();?>><!--body_class() is a helper function provided by WordPress that will output a list of useful CSS classes which describe the page-->
