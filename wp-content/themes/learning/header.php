f999d54e3aabe6c6dc8dfbe755b5ad48886645d6
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-24 10:33:09 +0545

    wordpress custom theme folder structure

--- wp-content/themes/learning/header.php
+++
@@ -1,8 +0,0 @@
-<header class="site-header">
-	<p class="site-title">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<?php bloginfo( 'name' ); ?>
-		</a>
-	</p>
-	<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-</header>
