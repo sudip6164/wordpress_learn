2e18450170c65aeb19bbb9abd7198fa56cca58e9
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-27 11:03:16 +0545

    archive

---
+++ wp-content/themes/learning/archive.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <?php get_template_part('includes/section', 'archive'); ?>
+</div>
+
+<?php get_footer(); ?>
--- wp-content/themes/learning/includes/section-archive.php
+++ wp-content/themes/learning/includes/section-archive.php
@@ -1,4 +1,5 @@
 <?php if (have_posts()): ?>
 	<?php while (have_posts()): the_post(); ?>
-            <?php the_content(); ?>
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
 	<?php endwhile; else: endif; ?>
