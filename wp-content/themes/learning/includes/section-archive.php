28a7fa3e0e7fdbc67ed96ab7d51eceb903349aa9
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-27 11:07:51 +0545

    read more permalink

--- wp-content/themes/learning/includes/section-archive.php
+++ wp-content/themes/learning/includes/section-archive.php
@@ -1,5 +1,6 @@
 <?php if (have_posts()): ?>
 	<?php while (have_posts()): the_post(); ?>
         <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
+        <?php the_excerpt(); ?>
+        <a href="<?php the_permalink(); ?>">Read more</a>
 	<?php endwhile; else: endif; ?>
