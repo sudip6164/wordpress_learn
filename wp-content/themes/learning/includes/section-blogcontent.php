e78970a465e130650c9d87f174bbe308aa56e2df
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-27 17:27:21 +0545

    post tags

--- wp-content/themes/learning/includes/section-blogcontent.php
+++ wp-content/themes/learning/includes/section-blogcontent.php
@@ -6,4 +6,11 @@
             <?php $fname = get_the_author_meta('first_name');?>
             <?php $lname = get_the_author_meta('last_name');?>
             <p>Posted by <?php echo $fname ?> <?php echo $lname ?></p>
+            <?php $tags = get_the_tags();
+            if($tags):
+            foreach ($tags as $tag): ?>
+                <a href="<?php echo get_tag_link($tag->term_id); ?>">
+                    <?php echo $tag->name?>
+                </a>
+            <?php endforeach; endif; ?>
 	<?php endwhile; else: endif; ?>
