47075f692c4902fca295a5deed33f2b9876483f0
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-27 11:32:15 +0545

    pagination first way

--- wp-content/themes/learning/archive.php
+++ wp-content/themes/learning/archive.php
@@ -2,6 +2,8 @@
 
 <div class="container">
     <?php get_template_part('includes/section', 'archive'); ?>
+    <?php previous_posts_link(); ?>
+    <?php next_posts_link(); ?>
 </div>
 
 <?php get_footer(); ?>
