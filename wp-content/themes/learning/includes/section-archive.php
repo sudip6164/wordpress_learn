1fed186c810fadf3ef9ec70a31d48e06c8d99a2a
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-27 11:22:38 +0545

    style archive

--- wp-content/themes/learning/includes/section-archive.php
+++ wp-content/themes/learning/includes/section-archive.php
@@ -1,6 +1,11 @@
 <?php if (have_posts()): ?>
-	<?php while (have_posts()): the_post(); ?>
-        <h1><?php the_title(); ?></h1>
-        <?php the_excerpt(); ?>
-        <a href="<?php the_permalink(); ?>">Read more</a>
-	<?php endwhile; else: endif; ?>
+    <?php while (have_posts()):
+        the_post(); ?>
+        <div class="card mb-3">
+            <div class="card-body">
+                <h1><?php the_title(); ?></h1>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>" class="btn btn-success">Read more</a>
+            </div>
+        </div>
+    <?php endwhile; else: endif; ?>
