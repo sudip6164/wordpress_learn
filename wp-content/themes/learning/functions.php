59cb31cf52ed6e78515da0a791abec8ba6801460
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-28 13:59:17 +0545

    classic editor and classic menus plugin

--- wp-content/themes/learning/functions.php
+++ wp-content/themes/learning/functions.php
@@ -31,3 +31,24 @@
 
     add_image_size('blog-large', 800, 600, false);
     add_image_size('blog-small', 300, 200, true);
+
+    function my_sidebars(){
+        register_sidebar(
+            array(
+                'name' => 'Page Sidebar',
+                'id' => 'page-sidebar',
+                'before_title' => '<h4 class="widget-title">',
+                "after_title" => '</h4>'
+            )
+        );
+
+        register_sidebar(
+            array(
+                'name' => 'Blog Sidebar',
+                'id' => 'blog-sidebar',
+                'before_title' => '<h4 class="widget-title">',
+                "after_title" => '</h4>'
+            )
+        );
+    }
+    add_action('widgets_init', 'my_sidebars');
