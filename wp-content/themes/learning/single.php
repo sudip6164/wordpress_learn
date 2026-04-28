876c73809ba7ebdbe28f2e69f72d7c9f7b80c6a7
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-28 11:20:41 +0545

    link pages of same post

--- wp-content/themes/learning/single.php
+++ wp-content/themes/learning/single.php
@@ -1,8 +1,10 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <h1><?php the_title();?></h1>
+    <h1><?php the_title(); ?></h1>
     <?php get_template_part('includes/section', 'blogcontent'); ?>
 </div>
+
+<?php wp_link_pages(); ?>
 
 <?php get_footer(); ?>
