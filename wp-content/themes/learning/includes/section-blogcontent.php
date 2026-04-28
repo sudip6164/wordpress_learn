de24e7faafc78bb08f45559e6954a4b2e5eb5296
sudip6164 <sudippradhanadgj@gmail.com>
2026-04-28 10:45:48 +0545

    category in blog post

--- wp-content/themes/learning/includes/section-blogcontent.php
+++ wp-content/themes/learning/includes/section-blogcontent.php
@@ -1,16 +1,28 @@
 <?php if (have_posts()): ?>
-	<?php while (have_posts()): the_post(); ?>
-            <?php echo get_the_date('Y-m-d'); ?>
-            <?php the_content(); ?>
-            <?php // the_author(); ?>
-            <?php $fname = get_the_author_meta('first_name');?>
-            <?php $lname = get_the_author_meta('last_name');?>
-            <p>Posted by <?php echo $fname ?> <?php echo $lname ?></p>
-            <?php $tags = get_the_tags();
-            if($tags):
+    <?php while (have_posts()):
+        the_post(); ?>
+        <?php echo get_the_date('Y-m-d'); ?>
+        <?php the_content(); ?>
+        <?php // the_author(); ?>
+        <?php $fname = get_the_author_meta('first_name'); ?>
+        <?php $lname = get_the_author_meta('last_name'); ?>
+        <p>Posted by <?php echo $fname ?>         <?php echo $lname ?></p>
+
+        <?php $tags = get_the_tags();
+        if ($tags):
             foreach ($tags as $tag): ?>
                 <a href="<?php echo get_tag_link($tag->term_id); ?>">
-                    <?php echo $tag->name?>
+                    <?php echo $tag->name ?>
                 </a>
             <?php endforeach; endif; ?>
-	<?php endwhile; else: endif; ?>
+
+        <?php $categories = get_the_category();
+        if ($categories):
+            echo("Category: ");
+            foreach ($categories as $cat): ?>
+                <a href="<?php echo get_category_link($cat->term_id); ?>">
+                    <?php echo $cat->name ?>
+                </a>
+            <?php endforeach; endif; ?>
+
+    <?php endwhile; else: endif; ?>
